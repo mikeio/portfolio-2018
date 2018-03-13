45e79140f4e8c8003c5a075e423230354ff48682
Mike Peters <mapdesigns@gmail.com>
2018-03-12 22:28:37 -0400

    start animations

--- js/scripts.js
+++ js/scripts.js
@@ -1,24 +1,61 @@
+
 $(document).ready(function(){
-  // Add smooth scrolling to all links
-  $("a").on('click', function(event) {
 
-    // Make sure this.hash has a value before overriding default behavior
-    if (this.hash !== "") {
-      // Prevent default anchor click behavior
-      event.preventDefault();
+	// Smooth Scroll
+	$("a").on('click', function(event) {
+		if (this.hash !== "") {
+			event.preventDefault();
+		var hash = this.hash;
+		$('html, body').animate({
+			scrollTop: $(hash).offset().top + 25
+		}, 1000, function(){
+		});
+	} 
+	});
 
-      // Store hash
-      var hash = this.hash;
+	$('.menu, #mobile-nav a').on('click', function() {
+        $('.menu').toggleClass('menu-open');
+        $('#mobile-nav').fadeToggle();
+        $("body").toggleClass("overflow-hidden");
+    });
 
-      // Using jQuery's animate() method to add smooth page scroll
-      // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
-      $('html, body').animate({
-        scrollTop: $(hash).offset().top -70
-      }, 1000, function(){
-
-      });
-    } // End if
-  });
 });
 
-var rellax = new Rellax('.rellax');
+$(window).load(function() {
+
+	// Hide Loader
+	$("#loading img").fadeOut(600);
+	$("#loading").delay(300).fadeOut(600);
+
+	// Block Reveal
+	$('#home h1').addClass('pre_appear')
+  		setTimeout(function(){
+    	$('#home h1').removeClass('pre_appear')
+  	}, 600)
+
+	// Wow.js
+	var wow = new WOW({
+        boxClass: 'wow', // animated element css class (default is wow)
+        animateClass: 'animated', // animation css class (default is animated)
+        offset: 100, // distance to the element when triggering the animation (default is 0)
+        mobile: false, // trigger animations on mobile devices (default is true)
+        live: true, // act on asynchronously loaded content (default is true)
+        callback: function(box) {
+            // the callback is fired every time an animation is started
+            // the argument that is passed in is the DOM node being animated
+        },
+        scrollContainer: null // optional scroll container selector, otherwise use window
+    });
+    wow.init();
+
+    // Rellax.js
+    var rellax = new Rellax('.rellax');
+
+});
+
+// Hide Down Arrow
+$(window).scroll(function(){
+	$("#scroll_down").css("opacity", 1 - $(window).scrollTop() / 350);
+});
+
+
